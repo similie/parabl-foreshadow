e32883850bbc4fdbac525ea145fcf14182d6e690
Adam Smith <adam.smith@similie.org>
2025-03-18 17:00:48 +0900

    chore: updating readme

--- libs/config.ts
+++ libs/config.ts
@@ -1,13 +1,14 @@
 import Constants from "expo-constants";
 export const httpServer =
   Constants.expoConfig?.extra?.parablForeshadowApiHost ||
-  "https://foreshadow.parabl.io"; // "http://localhost:1612";
-// export const tileServer = "http://prime.local:5001";
+  "https://foreshadow.parabl.io";
+
 export const API_ROUTE = Constants.expoConfig?.extra?.apiBasePath || "/api/v2/";
 export const tileServer = `${httpServer}${API_ROUTE}weather`;
 
 export const getApiUrl = (...route: string[]) => {
-  return `${httpServer}${API_ROUTE}${route.join("/")}`;
+  const url = `${httpServer}${API_ROUTE}${route.join("/")}`;
+  return url;
 };
 export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const phoneRegex = /^\+?[1-9]\d{1,14}$/; // E.164 format
